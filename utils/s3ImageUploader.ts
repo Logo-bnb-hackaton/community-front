5bb4965473b146e474a9d530dd24aacebde93c28
Leonid Zemenkov <zmaster-leo@yandex.ru>
2023-05-13 15:36:57 +0300

    fix

--- utils/s3ImageUploader.ts
+++ utils/s3ImageUploader.ts
@@ -3,13 +3,7 @@ import {profileBucket, subscriptionBucket} from "@/utils/s3";
 // @ts-ignore
 import {v4 as uuidv4} from 'uuid';
 
-const s3 = new S3Client({
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!!,
-    },
-    region: process.env.AWS_REGION!!
-})
+const s3 = new S3Client({})
 
 export async function uploadProfileImage(base64Image: string): Promise<string | undefined> {
     return await uploadToS3(profileBucket, base64Image);
@@ -21,6 +15,7 @@ export async function uploadSubscriptionImage(base64Image: string): Promise<stri
 
 async function uploadToS3(bucket: string, base64Image: string): Promise<string | undefined> {
     console.log(`Start saving new image`);
+    console.log(process.env);
 
     const type: string = base64Image.substring("data:image/".length, base64Image.indexOf(";base64"));
 
