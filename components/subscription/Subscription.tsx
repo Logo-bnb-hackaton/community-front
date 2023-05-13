d6e28a849409f788be409b23659a4a2ff8f844fe
Leonid Zemenkov <zmaster-leo@yandex.ru>
2023-05-13 21:47:24 +0300

    fix tg bot link

--- components/subscription/Subscription.tsx
+++ components/subscription/Subscription.tsx
@@ -30,6 +30,8 @@ interface Props {
     tgLinkStatus?: TgChatStatusDTO
 }
 
+const tg_bot = 'https://t.me/nodde_bot';
+
 const Subscription: React.FC<Props> = (
     {
         subscription,
@@ -157,7 +159,7 @@ const Subscription: React.FC<Props> = (
         if (tgLinkStatus.status === GetInviteLinkStatusType.CODE_GENERATED) {
             return (
                 <CopyToClipboard text={tgLinkStatus.code!!}>
-                    <Link href="https://www.t.me/sprut_signals_bot" target={'_blank'}>
+                    <Link href={tg_bot} target={'_blank'}>
                         Copy invite code and Go to telegram
                     </Link>
                 </CopyToClipboard>
@@ -166,7 +168,7 @@ const Subscription: React.FC<Props> = (
         if (tgLinkStatus.status === GetInviteLinkStatusType.NOT_GENERATED) return <p>Generate invite code</p>;
         if (tgLinkStatus.status === GetInviteLinkStatusType.CODE_USED) {
             return (
-                <Link href="https://www.t.me/sprut_signals_bot" target={'_blank'}>
+                <Link href={tg_bot} target={'_blank'}>
                     Go to telegram
                 </Link>
             );
